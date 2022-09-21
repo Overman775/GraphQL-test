5b67ec0a15bce1546d503bfc110f4aa7113ba62b
Alexey Zdorovykh <47769319+Overman775@users.noreply.github.com>
2022-09-21 09:33:50 +0500

    dio provider

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,16 +1,26 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../modules/home/home_page.dart';
+import 'services/dio/dio.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        Provider<Dio>(
+          create: (context) => AppDio().initDio(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
---
+++ lib/app/services/dio/dio.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:dio_http2_adapter/dio_http2_adapter.dart';
+import 'package:flutter/foundation.dart';
+
+import 'adapters/http_compress_adapter.dart';
+
+class AppDio {
+  Dio initDio() {
+    final dio = Dio();
+
+    dio.httpClientAdapter = HttpCompressAdapter(
+      Http2Adapter(
+        ConnectionManager(
+          // Ignore bad certificate
+          onClientCreate: (_, config) {
+            config
+              ..onBadCertificate = _onBadCertificate
+              ..proxy = _getProxy();
+          },
+        ),
+      ),
+    );
+
+    if (kDebugMode) {
+      dio.interceptors.add(
+        LogInterceptor(
+          requestBody: true,
+          responseBody: true,
+        ),
+      );
+    }
+
+    return dio;
+  }
+
+  bool _onBadCertificate(X509Certificate _) => kDebugMode;
+
+  Uri? _getProxy() {
+    const String? proxyUrl = null;
+
+    return proxyUrl != null
+        ? proxyUrl.startsWith('http://') || proxyUrl.startsWith('https://')
+            ? Uri.parse(proxyUrl)
+            : Uri.parse('http://$proxyUrl')
+        : null;
+  }
+}
