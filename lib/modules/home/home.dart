1001c2f7ac84f767da40098db4cb10d9153bb095
Alexey Zdorovykh <47769319+Overman775@users.noreply.github.com>
2022-09-29 18:53:14 +0500

    added models and mapers

--- lib/modules/home/home.dart
+++ lib/modules/home/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/widgets.dart';
 import 'package:graphql_test_pos/modules/home/screens/pages/home_page.dart';
 import 'package:provider/provider.dart';
 
-import 'services/client/home_client.dart';
 import 'services/client/home_graphql_client.dart';
 
 class Home extends StatelessWidget {
@@ -10,15 +9,8 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<HomeClient>(
-          create: (context) => HomeGraphQLClient(context.read()),
-        ),
-      ],
-      child: HomePage(
-        repository: context.read(),
-      ),
+    return HomePage(
+      repository: HomeGraphQLClient(context.read()),
     );
   }
 }
